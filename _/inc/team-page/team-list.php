36c20bc96fc2d1e22fc473eecce7d8de721eceee
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2015-01-27 16:38:14 +0000

    Tuesday 27 January 2015 - Add social like to team profiles.

--- _/inc/team-page/team-list.php
+++ _/inc/team-page/team-list.php
@@ -1,5 +1,7 @@
 <?php if (!empty($team)) { ?>
-	<?php foreach ($team as $tm) { ?>
+	<?php foreach ($team as $tm) { 
+	$social_links = $tm['social_links'];
+	?>
 	<article class="profile-box">
 		
 		<?php if ($tm['tm_img']) { ?>
@@ -13,9 +15,21 @@
 			<strong><?php echo $tm['tm_position']; ?></strong>
 			<a href="mailto:<?php echo $tm['tm_email']; ?>" title="View: <?php echo $child->post_title; ?>"><i class="fa fa-envelope"></i> <?php echo $tm['tm_email']; ?></a>
 		</header>
+		
+		<?php if (count($social_links > 0)) { ?>
+		<div class="social-links">
+			
+			<?php foreach ($social_links as $social_link) { ?>
+			<a href="<?php echo $social_link['social_url'] ; ?>" target="_blank"><i class="fa <?php echo $social_link['social_icon'] ; ?> fa-2x"></i></a>	
+			<?php } ?>
+
+		</div>
+		<?php } ?>
+		
 		<div class="pb-entry">
 			<?php echo $tm['tm_biog']; ?>
 		</div>
+		
 	</article>
 	<?php } ?>
 <?php } ?>
